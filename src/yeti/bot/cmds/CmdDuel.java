0c77e108512842c1a02744793572f137234c79bc
ZedTheYeti <zedtheyeti@gmail.com>
2015-03-13 18:34:38 -0500

    * Restructured the CmdDuel code and added a timeout of 2 minutes to duels (AKA they'll get cleared if they aren't accepted/declined)

--- src/yeti/bot/cmds/CmdDuel.java
+++ src/yeti/bot/cmds/CmdDuel.java
@@ -8,12 +8,16 @@ import yeti.bot.util.Util;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Z on 1/15/2015.
  */
 public class CmdDuel extends Command
 {
+   private static final long DUEL_TIMEOUT = 2 * 60 * 1000; // 2 minutes in milliseconds
+
    private HashMap<String, ArrayList<DuelInfo>> duels = new HashMap<>();
 
    class DuelInfo
@@ -31,6 +35,39 @@ public class CmdDuel extends Command
       }
    }
 
+   public CmdDuel()
+   {
+      TimerTask task = new TimerTask()
+      {
+         @Override
+         public void run()
+         {
+            for(ArrayList<DuelInfo> list : duels.values())
+            {
+               if(list == null)
+                  continue;
+
+               long currTime = System.currentTimeMillis();
+
+               for(int i = 0; i < list.size(); i++)
+               {
+                  DuelInfo info = list.get(i);
+
+                  if(currTime - info.time >= DUEL_TIMEOUT)
+                  {
+                     Logger.logDebug("Clearing duel between " + info.challenger.getName() + " and " + info.opponent.getName());
+                     list.remove(i);
+                     i--;
+                  }
+               }
+            }
+         }
+      };
+      Timer t = new Timer();
+      // Wait DUEL_TIMEOUT, then execute every 30 seconds
+      t.scheduleAtFixedRate(task, DUEL_TIMEOUT, 30 * 1000);
+   }
+
    @Override
    public boolean check(String user, String cmd, boolean isSub)
    {
@@ -41,75 +78,94 @@ public class CmdDuel extends Command
    @Override
    public void process(String user, String msg)
    {
-      if (msg.startsWith("!duel"))
+      User usr = Globals.getOnlineUser(user);
+      String[] parts = msg.split(" ");
+
+      if(parts.length == 0 || usr == null)
+         return;
+
+      parts[0] = parts[0].trim().toLowerCase();
+
+      switch(parts[0])
       {
-         Logger.logDebug("DUEL " + msg);
-         String[] parts = msg.split(" ");
+         case "!duel": processDuel(usr, parts); break;
+         case "!accept": processAnswer(usr, true, parts); break;
+         case "!decline": processAnswer(usr, false, parts); break;
+      }
+   }
 
-         if (parts.length < 2)
-            return;
+   private void processDuel(User user, String[] parts)
+   {
+      if (parts.length < 2)
+         return;
 
-         User challenger = Globals.getOnlineUser(user),
-               opponent = Globals.getOnlineUser(parts[1]);
+      User challenger = user,
+            opponent = Globals.getOnlineUser(parts[1]);
 
-         if (challenger == null || opponent == null)
-         {
-            Logger.logDebug("DUEL user not found");
-            return;
-         }
-
-         if(challenger == opponent || opponent.getName() == Globals.username)
-            return;
-
-         int amount;
-         try
-         {
-            if(parts.length >= 3)
-               amount = Integer.parseInt(parts[2]);
-            else
-               amount = 0;
-         } catch (NumberFormatException exc)
-         {
-            exc.printStackTrace();
-            Logger.logDebug("DUEL incorrect number format");
-            return;
-         }
-
-         if(amount < 0)
-            return;
-
-         if (challenger.getExp() < amount)
-         {
-            JIRC.sendMessage(Globals.channel, "You don't have enough xp for that wager " + challenger.getName() + "!");
-            return;
-         } else if (opponent.getExp() < amount)
-         {
-            JIRC.sendMessage(Globals.channel, opponent.getName() + " doesn't have enough xp for that wager, " + challenger.getName() + ".");
-            return;
-         }
-
-         ArrayList<DuelInfo> infoList = duels.get(opponent.getName());
-         if(infoList == null)
-         {
-            infoList = new ArrayList<>();
-            duels.put(opponent.getName(), infoList);
-         }
-         infoList.add(new DuelInfo(challenger, opponent, amount, System.currentTimeMillis()));
-         JIRC.sendMessage(Globals.channel, "/me " + challenger.getName() + " has challenged " + opponent.getName() + " to a duel for " + amount + " xp! " + opponent.getName() + ", use !accept or !decline to answer the challenge.");
-      } else if (msg.startsWith("!accept"))
+      if (challenger == null || opponent == null)
       {
-         User usr = Globals.getOnlineUser(user);
+         Logger.logDebug("DUEL user not found");
+         return;
+      }
 
-         if (usr == null)
-            return;
+      if(challenger == opponent || opponent.getName() == Globals.username)
+         return;
 
-         ArrayList<DuelInfo> infoList = duels.get(usr.getName());
-         if(infoList != null)
+      int amount;
+      try
+      {
+         if(parts.length >= 3)
+            amount = Math.max(0, Integer.parseInt(parts[2]));
+         else
+            amount = 0;
+      } catch (NumberFormatException exc)
+      {
+         Logger.logDebug("DUEL incorrect number format, using 0xp");
+         return;
+      }
+
+      if(amount < 0)
+         return;
+
+      if (challenger.getExp() < amount)
+      {
+         JIRC.sendMessage(Globals.channel, "You don't have enough xp for that wager " + challenger.getName() + "!");
+         return;
+      } else if (opponent.getExp() < amount)
+      {
+         JIRC.sendMessage(Globals.channel, opponent.getName() + " doesn't have enough xp for that wager, " + challenger.getName() + ".");
+         return;
+      }
+
+      ArrayList<DuelInfo> infoList = duels.get(opponent.getName().toLowerCase());
+      if(infoList == null)
+      {
+         infoList = new ArrayList<>();
+         duels.put(opponent.getName().toLowerCase(), infoList);
+      }
+      infoList.add(new DuelInfo(challenger, opponent, amount, System.currentTimeMillis()));
+      JIRC.sendMessage(Globals.channel, "/me " + challenger.getName() + " has challenged " + opponent.getName() + " to a duel for " + amount + " xp! " + opponent.getName() + ", use !accept or !decline to answer the challenge.");
+   }
+
+   private void processAnswer(User usr, boolean accepted, String parts[])
+   {
+      if (usr == null)
+         return;
+
+      ArrayList<DuelInfo> infoList = duels.get(usr.getName().toLowerCase());
+      if(infoList != null)
+      {
+         DuelInfo info = infoList.remove(0);
+         if (infoList.isEmpty())
+            duels.remove(usr.getName().toLowerCase());
+         if (info == null || info.challenger == null || info.opponent == null)
          {
-            DuelInfo info = infoList.remove(0);
-            if(infoList.isEmpty())
-               duels.remove(usr.getName());
+            Logger.logDebug("ERROR DUEL !accept/!decline: " + parts + "\n" + info + "\n" + info.challenger + "\n" + info.opponent);
+            return;
+         }
 
+         if(accepted)
+         {
             int challengerRoll = Util.rollDie(20), opponentRoll = Util.rollDie(20);
 
             JIRC.sendMessage(Globals.channel, "/me " + info.opponent.getName() + " has accepted " + info.challenger.getName() + "'s challenge! A vicious battle ensues.");
@@ -160,22 +216,8 @@ public class CmdDuel extends Command
                JIRC.sendMessage(Globals.channel, bldr.toString());
             }).start();
          }
-      } else if (msg.startsWith("!decline"))
-      {
-         User usr = Globals.getOnlineUser(user);
-
-         if (usr == null)
-            return;
-
-         ArrayList<DuelInfo> infoList = duels.get(usr.getName());
-         if(infoList != null)
-         {
-            DuelInfo info = infoList.remove(0);
-            if(infoList.isEmpty())
-               duels.remove(usr.getName());
-
+         else
             JIRC.sendMessage(Globals.channel, info.opponent.getName() + " has declined " + info.challenger.getName() + "'s challenge.");
-         }
       }
    }
 
